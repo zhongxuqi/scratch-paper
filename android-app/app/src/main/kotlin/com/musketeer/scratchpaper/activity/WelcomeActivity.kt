edcfcb929f3ab79df089cbd2e383ffe7142dc565
zhongxuqi <zhongxuqi.1991@gmail.com>
2017-06-14 10:21:12 +0800

    update android project

---
+++ android-app/app/src/main/kotlin/com/musketeer/scratchpaper/activity/WelcomeActivity.kt
@@ -0,0 +1,28 @@
+package com.musketeer.scratchpaper
+
+import android.os.Bundle
+import com.musketeer.baselibrary.Activity.BaseActivity
+
+class WelcomeActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+
+    override fun setContentView(savedInstanceState: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initEvent() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
