35b52d68fda44cf84ffffd4e89463a9e004a14ae
zhongxuqi <zhongxuqi.1991@gmail.com>
2017-06-19 10:01:40 +0800

    add welcome page

--- android-app/app/src/main/kotlin/com/musketeer/scratchpaper/activity/WelcomeActivity.kt
+++ android-app/app/src/main/kotlin/com/musketeer/scratchpaper/activity/WelcomeActivity.kt
@@ -1,14 +1,30 @@
-package com.musketeer.scratchpaper
+package com.musketeer.scratchpaper.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
-import android.widget.ImageView
-import com.musketeer.scratchpaper.activity.BaseActivity
-import com.musketeer.scratchpaper.activity.MainActivity
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.musketeer.scratchpaper.R
+import com.musketeer.scratchpaper.utils.LogUtils
+import com.qq.e.ads.splash.SplashAD
+import com.qq.e.ads.splash.SplashADListener
+
 
 class WelcomeActivity : BaseActivity() {
+    companion object {
+        val TAG = "WelcomeActivity"
+        val REQUEST_PERMISSIONS = 0
+        val handler = Handler()
+    }
 
-    private var mWelcomeView: ImageView? = null
+    private var mWelcomeView: RelativeLayout? = null
+    private var mSkipButton: TextView? = null
+    private var hasSkip: Boolean = false
 
     override fun setContentView(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_welcome)
@@ -16,19 +32,90 @@ class WelcomeActivity : BaseActivity() {
 
     override fun initView() {
         supportActionBar!!.hide()
-        mWelcomeView = findViewById(R.id.welcome_image) as ImageView
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), REQUEST_PERMISSIONS)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSIONS)
+        }
+        mWelcomeView = findViewById(R.id.adcontent) as RelativeLayout
+        val splashAD = SplashAD(this, mWelcomeView!!, View(this), "1103577955", "8040328324309898", object: SplashADListener{
+            override fun onNoAD(ecode: Int) {
+                LogUtils.d(TAG, "ecode:$ecode")
+            }
+
+            override fun onADDismissed() {
+                LogUtils.d(TAG, "onADDismissed")
+            }
+
+            override fun onADPresent() {
+                LogUtils.d(TAG, "onADPresent")
+            }
+
+            override fun onADClicked() {
+                LogUtils.d(TAG, "onADClicked")
+            }
+
+            override fun onADTick(p0: Long) {
+                LogUtils.d(TAG, "onADTick:$p0")
+            }
+        }, 0)
+        mSkipButton = findViewById(R.id.skip_button) as TextView
     }
 
     override fun initEvent() {
-        Handler().postDelayed(object: Runnable{
-            override fun run() {
-                startActivity(MainActivity::class.java)
-                finish()
-            }
-        }, 2000)
+        mSkipButton?.setOnClickListener {
+            start()
+        }
+        handler.post(CloseRunner(3))
     }
 
     override fun initData() {
-        mWelcomeView!!.setImageResource(R.mipmap.welcome)
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSIONS -> {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), REQUEST_PERMISSIONS)
+                    return
+                }
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSIONS)
+                    return
+                }
+                startActivity(WelcomeActivity::class.java)
+                finish()
+            }
+        }
+    }
+
+    fun start() {
+        synchronized(this) {
+            if (!hasSkip) {
+                startActivity(MainActivity::class.java)
+                finish()
+                this.hasSkip = true
+            }
+        }
+    }
+
+    inner class CloseRunner(time: Int): Runnable {
+        var totalTime: Int
+
+        init {
+            totalTime = time
+        }
+
+        override fun run() {
+            this.totalTime--
+            this@WelcomeActivity.mSkipButton?.setText("点击跳过 (${totalTime}s)")
+            if (this.totalTime > 0) {
+                handler.postDelayed(this, 1000)
+            } else {
+                start()
+            }
+        }
     }
 }
