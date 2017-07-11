7e29dd677758ec421e4199ce7f9c41266faffa12
zhongxuqi <zhongxuqi.1991@gmail.com>
2017-07-11 19:20:42 +0800

    fix bug of BrowsePaperActivity

--- android-app/app/src/main/kotlin/com/musketeer/scratchpaper/activity/BrowsePaperActivity.kt
+++ android-app/app/src/main/kotlin/com/musketeer/scratchpaper/activity/BrowsePaperActivity.kt
@@ -78,7 +78,7 @@ class BrowsePaperActivity : BaseActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
             Config.ACTION_EDIT_PAPER -> {
-                initData()
+//                initData()
 
                 val intent = Intent()
                 intent.action = "android.appwidget.action.APPWIDGET_UPDATE"
